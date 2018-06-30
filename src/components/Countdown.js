c51db81ec2362bfcf5ba1f74f65c0bf310703e1f
usarneme <jamestcoop@gmail.com>
2018-06-29 20:16:14 -0700

    Updated Countdown component: made buttons (start, pause, reset) function correctly, made buttons for preset lengths (pomodoro, short break, long break) function, updated styling to make it more mobile friendly and focused on the timer.

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,29 +2,69 @@ import React from 'react'
 import '../styles/Countdown.css'
 
 class Countdown extends React.Component {
+
   // props.timerLength contains the length of the timer in milliseconds:
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      currentCount: props.timerLength
+      maxTimerLength: props.timerLength,
+      currentCount: props.timerLength,
+      timerRunning: false
     }
     this.timer = this.timer.bind(this)
+    this.pause = this.pause.bind(this)
+    this.start = this.start.bind(this)
+    this.reset = this.reset.bind(this)
+    this.setTimerTo = this.setTimerTo.bind(this)
   }
 
   timer() {
     this.setState({
       currentCount: this.state.currentCount - 1
     })
-    if(this.state.currentCount < 1) { 
+    if (this.state.currentCount < 1) { 
       clearInterval(this.intervalId)
     }
   }
 
-  componentDidMount() {
-    this.intervalId = setInterval(this.timer, 1000)
+  pause() {
+    if (this.state.timerRunning) {
+      this.setState({
+        timerRunning: false
+      })
+      clearInterval(this.intervalId)
+    } 
   }
 
-  componentWillUnmount(){
+  start() {
+    if (!this.state.timerRunning) {
+      this.setState({
+        timerRunning: true
+      })
+      this.intervalId = setInterval(this.timer, 1000)
+    }
+  }
+
+  reset() {
+    console.log('resetting to this.maxTimerLength '+this.state.maxTimerLength)
+    clearInterval(this.intervalId)
+    this.setState({
+      timerRunning: false,
+      currentCount: this.state.maxTimerLength
+    })
+  }
+
+  setTimerTo(length = 1500) {
+    clearInterval(this.intervalId)
+    this.setState({
+      maxTimerLength: length,
+      timerRunning: false,
+      currentCount: length
+    })
+  }
+
+  // Prevent a memory leak by clearing the interval if the component ever gets unmounted
+  componentWillUnmount() {
     clearInterval(this.intervalId)
   }
 
@@ -32,9 +72,12 @@ class Countdown extends React.Component {
     return(
       <div className="countdownContainer">
         <div className="selector">
-          <span><button className="toggleSwitches">Pomodoro</button></span>
-          <span><button className="toggleSwitches">Short Break</button></span>
-          <span><button className="toggleSwitches">Long Break</button></span>
+        {
+          // pomodoro 1500, short break 300, long break 900 
+        }
+          <button className="toggleSwitches" onClick={() => this.setTimerTo(1500)} >Pomodoro</button>
+          <button className="toggleSwitches" onClick={() => this.setTimerTo(300)} >Short Break</button>
+          <button className="toggleSwitches" onClick={() => this.setTimerTo(900)} >Long Break</button>
         </div>
         <div className="countdown">
           <span className="minutes">
@@ -46,9 +89,9 @@ class Countdown extends React.Component {
           </span>
         </div>
         <div className="buttonsContainer">
-          <span><button className="controlButton stop">Stop</button></span>
-          <span><button className="controlButton start">Start</button></span>
-          <span><button className="controlButton reset">Reset</button></span>
+          <button className="controlButton pause" onClick={this.pause} >Pause</button>
+          <button className="controlButton start" onClick={this.start} >Start</button>
+          <button className="controlButton reset" onClick={this.reset} >Reset</button>
         </div>
       </div>
     )
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import '../styles/Schedule.css'
 
-const Schedule = (props) => {
-  return (
-    <div className="scheduleContainer">
-      <h2 className="scheduleHeader">Schedule</h2>
-      <ul className="scheduleUl">
-      { props.schedule.map((time, idx) => {
-        return <li key={idx} className="scheduleLi">
-          {time / 60} minutes
-          </li>
-        })
-      }
-      </ul>
-    </div>
-  )
+class Schedule extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      schedule: [...props.schedule],
+      currentTimer: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="scheduleContainer">
+        <h2 className="scheduleHeader">Schedule</h2>
+        <ul className="scheduleUl">
+        { this.state.schedule.map((time, idx) => {
+          return <li key={idx} className="scheduleLi">
+            {time / 60} minutes
+            </li>
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Schedule
